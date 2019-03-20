b9c27b9422262feff9d0cf2e13792629a6288935
Fatih Baltacı <baltacifatih14@gmail.com>
2019-03-20 11:39:10 +0300

    Update slackinformer.py

--- slackinformer/slackinformer.py
+++ slackinformer/slackinformer.py
@@ -4,9 +4,9 @@ import os
 from slackclient import SlackClient
 
 
-def send_slack_message(content, plot_path):
-    if not os.path.isfile(plot_path):
-        print("File does not exist. Path: " + str(plot_path))
+def send_slack_message(channel, title="", content, img_path):
+    if not os.path.isfile(img_path):
+        print("File does not exist. Path: " + str(img_path))
         return
 
     slack_token = os.getenv("SLACK_CLIENT_API_KEY")
@@ -14,10 +14,10 @@ def send_slack_message(content, plot_path):
 
     response = sc.api_call(
         'files.upload',
-        channels='#ml-cv-results',
+        channels=channel,
         as_user=True,
-        file=open(plot_path, 'rb'),
-        title='Epoch vs mAP',
+        file=open(img_path, 'rb'),
+        title=title,
         initial_comment=content
     )
 
